d1f80bdd86626372d71deeee709818e8f64a6cab
lopez <jerome_lopez@hotmail.Com>
2021-12-13 14:36:24 +0100

    merlin saut

--- index.js
+++ index.js
@@ -3,32 +3,81 @@ const counter = document.getElementById("counter");
 const merlinContainer = document.getElementById("merlinContainer");
 const merlin = document.getElementById("merlin");
 const toucan = document.getElementById("toucan");
+const life = document.getElementById("life");
+let counterLife=0;
+const bear = document.getElementById("bear");
+const mountain = document.getElementById("mountain");
+const sky = document.getElementById("sky");
+let timer, secondes = 0; 
 let isToucanFlying=false;
 let isLeftPosition=false;
+let isAttack;
 let isBubblesUp=false;
+let isJumping = false;
+let spaceHoldStatus = false;
+let scorpioLeft, bearLeft;
 let counterLogos=0;
 const mushroom = document.getElementById("mushroom");
+const mushroom2 = document.getElementById("mushroom2");
+const tile3 = document.getElementById("tile3");
 const chest = document.getElementById("chest");
 const scorpio = document.getElementById("scorpio");
+const scorpioTouched = document.getElementById("scorpioTouched");
+const bearTouched = document.getElementById("bearTouched");
 const bubbles = document.getElementById('bubbles');
 const angular = document.getElementById('angular');
+const typescript = document.getElementById('typescript');
 const nodeJS = document.getElementById('nodeJS');
 const js = document.getElementById('js');
 const merlinStep = 3;
+let positionMountain = parseInt(mountain.style.backgroundPositionX,10);
 const merlininitialLeft = parseInt(document.getElementById('merlinContainer').offsetLeft,10);
-const floorWidth=parseInt(document.getElementById('floor').offsetWidth,10);
+//const floorWidth=parseInt(document.getElementById('floor').offsetWidth,10);
+const floorWidth= 1855;
 const containerinitialBottom=parseInt(document.getElementById('merlinContainer').offsetTop,10);
-let animMerlinRight, animMerlinAttack;
+let animMerlinRight, animMerlinAttack, timerCollision, tIDBear;
 let isAnimated = false;
 
 function checkCollision(element1,element2){
     if ((element1.left < element2.left + element2.width) && (element1.left + element1.width > element2.left) && (element1.top < element2.top + element2.height) && (element1.height + element1.top > element2.top)) {
+        
         // collision détectée !
-     // methodes.popAngular();
+ 
      return true
     }
+ 
   return false
 }
+function parralaxLeft(){
+    //positionMountain = parseInt(mountain.style.backgroundPositionX,10);
+    if(isNaN(positionMountain)){
+        positionMountain=-20;
+}
+positionMountain=positionMountain-20;
+mountain.style.backgroundPositionX=positionMountain + "px";
+};
+function parralaxRight(){
+    //positionMountain = parseInt(mountain.style.backgroundPositionX,10);
+    if(isNaN(positionMountain)){
+        positionMountain=20;
+}
+positionMountain=positionMountain+20;
+mountain.style.backgroundPositionX=positionMountain + "px";
+};
+function animateBear() {
+    let positionBear = 256; 
+   
+    tIDBear = setInterval ( () => {bear.style.backgroundPosition = `-${positionBear}px 0px`; 
+    if (positionBear < 1536)
+    { positionBear = positionBear + 256;}
+    else
+    { positionBear = 256; }
+    let speedBear=bear.offsetLeft+15;
+    bear.style.left=speedBear+'px';
+    }, 100);
+    }; 
+function stopBear() {clearInterval(tIDBear);
+};
 function merlinInitalPosition(){
     merlin.style.backgroundImage = `url('${merlinMoves.idleBase[0].srcImg}')`;
        merlin.style.backgroundPositionX=merlinMoves.idleBase[0].BackgroundX;
@@ -41,7 +90,7 @@ function merlinInitalLeftPosition(){
 merlin.style.backgroundImage = `url('${merlinMoves.idleBase[1].srcImg}')`;
 merlinContainer.style.top=merlinMoves.idleBase[1].topContainer;
 merlinContainer.style.width=merlinMoves.idleBase[1].widhtContainer;
-merlin.style.backgroundPositionX=merlinMoves.idleBase[1].BackgroundX;
+merlin.style.backgroundPositionX='-880px';
 merlinContainer.style.height=merlinMoves.idleBase[1].heightContainer;
 merlin.style.backgroundPositionY=merlinMoves.idleBase[1].BagroundY;
 merlinContainer.style.top=merlinMoves.idleBase[1].topContainer;
@@ -70,26 +119,104 @@ function getObjectLength (object){
     if(typeof(object)!=="object") throw new Error('not an object')
     return Object.keys(object).length;
 }
+//jump
+function jump(jumpDuration, jumpHeight, multiplier){
+    if(multiplier>5){
+      return;
+    }
+    //jumpCount.textContent = multiplier;
+    merlin.style.backgroundImage=`url('assets/img/wizardpack/jump.png')`;
+    gsap.to(merlinContainer, jumpDuration*multiplier, {
+      y:jumpHeight*multiplier,
+      ease:Power3.easeOut,
+      onComplete:function(){
+        // end jump
+        gsap.to(merlinContainer, (jumpDuration*multiplier)*.8, {
+          y:0, 
+          ease:Power1.easeIn,
+          onComplete:function(){
+            isJumping = false;
+          }
+        });            
+      }
+    });
+    // continue adjusting height of jump if spacebar is held
+    //TweenMax depredicated gsap
+    gsap.delayedCall(.06, function(){
+        if(spaceHoldStatus===true){    
+          multiplier += 1;
+          if(multiplier<=5){
+            jump(jumpDuration, jumpHeight, multiplier);
+          }
+        }            
+    });   
+  } 
+
 function checkObstacleCollision(obstacle,merlin){
-    setInterval(() => {
-        
-    }, 100);
+    //console.log(obstacle)
     const isACollision = checkCollision(obstacle,merlin);
-    
-    if(isACollision){
+    if((isACollision)&&(obstacle.enemy===false)){
+        console.log('merlin' + merlin.top+ 'left'+ merlin.left)
         obstacle.idTouched.style.display="block";
         obstacle.id.style.display="none";
         if (obstacle.touched===false){
-            methodes.manageScore();
             bubblesUp();
+            methodes.manageScore();
+            setTimeout(() => {
+                /*document.getElementById("merlin").style.backgroundImage=`url('assets/img/wizardpack/jump.png')`;*/
+                //brillance aux touchés des bienfaits
+                document.getElementById("merlin").style.filter="brightness(1.75)";
+                mountain.style.backgroundImage = `url('assets/img/glacial_mountains/background_glacial_mountains_lightened.png')`;
+            }, 50);
+            
         }
         obstacle.touched=true;
-    }else{
+    }
+    if((isACollision)&&(obstacle.enemy===true)){
+        if((directions.enterAttack)){
+            obstacle.id.style.display="none";
+            if (obstacle.touched===false){
+                bubblesUp();
+                methodes.manageScore();
+                //image à la position du scorpio
+                if(obstacle.id==='bear'){
+                    stopBear();
+                }
+                obstacle.idTouched.style.display="block";
+                obstacle.idTouched.style.left=document.getElementById("merlinContainer").offsetLeft;
+            }
+            obstacle.touched=true;
+        }else{
+            isAttack=false;
+            if((!isAttack)&&(!obstacle.touched)){
+               methodes.manageLife(); 
+            setTimeout(() => {
+                document.getElementById("merlin").style.opacity = "0.5";
+            }, 50);
+            isAttack=true;
+        }  
+        }
+        
+    }
+    /* remettre image de base pour obstacles
+    else{
         obstacle.idTouched.style.display="none";
         obstacle.id.style.display="block";
-    }
+        obstacles[2].id.style.display="none";
+        obstacles[2].idTouched.style.display="block";
+    }*/
 }
+
+//avoid scroll
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 function bubblesUp(){
+        bubbles.style.left= document.getElementById("merlinContainer").offsetLeft+'px';
         bubbles.style.display="block";
         bubbles.classList.add("bubblesUp");
         isBubblesUp=true;
@@ -99,15 +226,19 @@ function bubblesUp(){
         }
             if(counterLogos===2){
                 js.style.display="block";
+                //bear attack after JS
+                animateBear();
             } 
             if(counterLogos===3){
                 nodeJS.style.display="block";
             } 
+            if(counterLogos===4){
+                typescript.style.display="block";
+            }
         setTimeout(()=>{         
             bubbles.classList.remove("bubblesUp");
             bubbles.style.display="none";
-           
-    }, 4000);
+    }, 3000);
     
 }
 /*
@@ -127,6 +258,8 @@ const obstacles=[{
     id: document.getElementById('chest'),
     idTouched:document.getElementById('chestTouched'),
     touched:false,
+    class:'',
+    enemy:false,
 },
 {
     left: parseInt(mushroom.offsetLeft,10),
@@ -136,21 +269,46 @@ const obstacles=[{
     id: document.getElementById('mushroom'),
     idTouched:document.getElementById('mushroomTouched'),
     touched:false,
+    class:'',
+    enemy:false,
 },
 {
-    left: parseInt(scorpio.offsetLeft,10),
     top: parseInt(scorpio.offsetTop,10),
     width: parseInt(scorpio.offsetWidth,10),
     height: parseInt(scorpio.offsetHeight,10),
     id: document.getElementById('scorpio'),
     idTouched:document.getElementById('scorpioTouched'),
     touched:false,
-}]
+    class:'',
+    enemy:true,
+},
+{
+    top: parseInt(bear.offsetTop,10),
+    width: parseInt(bear.offsetWidth,10),
+    height: parseInt(bear.offsetHeight,10),
+    id: document.getElementById('bear'),
+    idTouched:document.getElementById('bearTouched'),
+    touched:false,
+    class:'',
+    enemy:true,
+},
+{
+    left: parseInt(mushroom2.offsetLeft,10),
+    top: parseInt(mushroom2.offsetTop,10),
+    width: parseInt(mushroom2.offsetWidth,10),
+    height: parseInt(mushroom2.offsetHeight,10),
+    id: document.getElementById('mushroom2'),
+    idTouched:document.getElementById('mushroom2Touched'),
+    touched:false,
+    class:'',
+    enemy:false,
+}
+]
 //perso objet
 const merlinMoves={
         idleBase:[{
             srcImg:'assets/img/wizardpack/idle.png',
-            BackgroundX: '-774px',
+            BackgroundX: '-79px',
             BagroundY: '-51px',
             widhtContainer: '60px',
             heightContainer: '90px',
@@ -158,7 +316,7 @@ const merlinMoves={
         },
         {
             srcImg:'assets/img/wizardpack/idle.png',
-            BackgroundX: '-880px',
+            BackgroundX: '-313px',
             BagroundY: '-51px',
             widhtContainer: '60px',
             heightContainer: '90px',
@@ -166,7 +324,7 @@ const merlinMoves={
         },
         {
             srcImg:'assets/img/wizardpack/idle.png',
-            BackgroundX: '-774px',
+            BackgroundX: '-544px',
             BagroundY: '-51px',
             widhtContainer: '69px',
             heightContainer: '90px',
@@ -174,7 +332,7 @@ const merlinMoves={
         },
         {
             srcImg:'assets/img/wizardpack/idle.png',
-            BackgroundX: '-774px',
+            BackgroundX: '-775px',
             BagroundY: '-51px',
             widhtContainer: '69px',
             heightContainer: '90px',
@@ -427,37 +585,56 @@ const directions = {
     top: false,
     left: false,
     right: false,
-    bottom: false,
+    space: false,
     enterAttack:false,
 };
 window.addEventListener('keydown', (event)=>{
     if('ArrowRight' === event.code){
         directions.right=true;
         methodes.onMerlinMove(); 
+        if(merlinContainer.offsetLeft<floorWidth){
+        parralaxLeft();
+        }
+        clouds.style.display="block";
+        clouds.classList.add("cloudsMove");
+        /*scorpio.style.display="block";
+        scorpio.classList.add("scorpioMove");*/
     }
     if('Enter' === event.code){
         directions.enterAttack=true;
-        methodes.onMerlinMove();  
+       methodes.onMerlinMove();  
     }
     if('ArrowLeft' === event.code){
         directions.left=true;
-        methodes.onMerlinMove();
+       methodes.onMerlinMove();
+       if(merlinContainer.offsetLeft>1){
+        parralaxRight();
+        }
+       
     } 
     if('ArrowUp' === event.code){
         directions.top=true;
     }
-    if('ArrowDown' === event.code){
-        directions.bottom=true;
+    if('Space' === event.code){
+        directions.space=true;
+        if(isJumping===false && spaceHoldStatus===false){
+            spaceHoldStatus = true;
+            isJumping = true;     
+            jump(.05, -40, 1);
+           
+          }
+        methodes.onMerlinMove();
+        
     }
 });
 window.addEventListener('keyup', (event)=>{
     if('ArrowRight' === event.code){
         directions.right=false;
         clearTimeout(animMerlinRight);
-       isAnimated = false;    
+       isAnimated = false;   
        merlinInitalPosition();
        isLeftPosition=false;
-        
+      
     }
     if('ArrowLeft' === event.code){
         directions.left=false;
@@ -475,13 +652,18 @@ window.addEventListener('keyup', (event)=>{
         }else{
         merlinInitalPosition();
     }
-
     } 
     if('ArrowUp' === event.code){
         directions.top=false;
     }
-    if('ArrowDown' === event.code){
-        directions.bottom=false;
+    if('Space' === event.code){
+        directions.space=false;
+        spaceHoldStatus = false;
+        if(isLeftPosition){
+            merlinInitalLeftPosition();
+         }else{
+         merlinInitalPosition();
+     }
     }
 
 });
@@ -510,7 +692,6 @@ let methodes = {
             }
             else {
                 merlin.style.transform = '';
-               
                 merlinContainer.style.width=`${merlinMoves.merlinRun[right].widhtContainer}`;
                 merlin.style.backgroundPosition = `${merlinMoves.merlinRun[right].BackgroundX} ${merlinMoves.merlinRun[right].BagroundY}`; 
                 merlinContainer.style.top=`${merlinMoves.merlinRun[right].topContainer}`;
@@ -530,7 +711,7 @@ let methodes = {
         merlinContainer.style.height=`${merlinMoves.merlinAttack[attack].heightContainer}`;
         merlin.style.backgroundImage=`url(${merlinMoves.merlinAttack[attack].srcImg})`;
         if (isLeftPosition){
-            merlin.style.transform = 'scaleX(-1)';               
+           merlin.style.transform = 'scaleX(-1)';               
             merlin.style.backgroundPosition = `${merlinMoves.merlinAttackLeft[attack].BackgroundX} ${merlinMoves.merlinAttackLeft[attack].BagroundY}`; 
             merlinContainer.style.width=`${merlinMoves.merlinAttackLeft[attack].widhtContainer}`;
             //merlin.style.backgroundImage=`url(assets/img/wizardpack/run.png)`;
@@ -559,17 +740,38 @@ let methodes = {
                     height:parseFloat(merlinContainer.offsetHeight),
                     width:parseFloat(merlinContainer.offsetWidth),
                 }
+                
+                //position scorpio collision
+                obstacles[2].left = parseInt(scorpio.offsetLeft,10);
+                scorpioLeft = parseInt(scorpio.offsetLeft,10);
+                //position bear collision
                 //window.scroll(left, 0);
+                obstacles[3].left = parseInt(bear.offsetLeft,10);
+                bearLeft = parseInt(bear.offsetLeft,10);
             obstacles.forEach(obstacle => {
             checkObstacleCollision(obstacle,currentMerlin); 
             }); 
-            
+            //after attack ennemy
+            document.getElementById("merlin").style.opacity = "1";
+            document.getElementById("merlin").style.filter="brightness(1)";
+            mountain.style.backgroundImage = `url('assets/img/glacial_mountains/background_glacial_mountains.png')`;
+            //merlinInitalPosition();
+            //after touch mushroom
+            /*document.getElementById("merlin").style.filter="brightness(1)";
+            mountain.style.backgroundImage = `url('assets/img/glacial_mountains/background_glacial_mountains.png')`;
+            merlinInitalPosition();*/
+
             toucanMoves(currentMerlin); 
-             
+           
             //settime out pour pas augmenter score à l'infini
                  /*setTimeout(()=>{
                     methodes.collision(obstaclesId)
                 }, 50);*/
+            },
+            manageLife: ()=>{
+                let widthLife=life.offsetWidth-56;
+                life.style.width=widthLife+'px';
+                counterLife+=1;
             },
             manageScore: ()=>{
             counter.innerHTML = parseInt(counter.innerHTML,10)+100;
@@ -585,7 +787,6 @@ let methodes = {
           }   
     }else {
         methodes.incrementPosition('left',-merlinStep);
-       
     }
     }
     if(directions.left){
@@ -612,19 +813,64 @@ let methodes = {
             methodes.incrementPosition('top',-merlinStep);
         }*/
     }
-    if(directions.bottom){
-        if(merlinContainer.offsetTop>containerinitialBottom){
+    if(directions.space){
+        /*if(merlinContainer.offsetTop>containerinitialBottom){
             methodes.incrementPosition('top',-merlinStep);
         }else{
         methodes.incrementPosition('top',merlinStep);
+    }*/
     }
-    }
+    //methodes.onMerlinMove();
     //methodes.toucanMove(divElement.offsetLeft);
     //methodes.collision();
     //recurssion compte à rebours 50ml secondes
     window.requestAnimationFrame(methodes.loopAnimation);
+    //Game over
+    if(counterLife>2){
+        alert('Vous pouvez réessayez si vous voulez. GAME OVER: attention aux animaux et autres ennemis!');
+        //edge chrome
+        counterLife=0;
+        document.location.reload();   
+        //edge
+        setTimeout(function(){ location.reload(); }, 2000);     
+    }
+   
 }
 };
 methodes.loopAnimation(0);
+//ok dans mozilla et pas chrome ni edge
+setInterval(methodes.onMerlinMove, 1000);
 
 
+function beginGame(){
+    /*alert('Pour jouer, appuyer sur les flèches Droite et Gauche. Pour attaquer appuyer sur Entrée. Pour passer directement au CV ou au site, cliquez sur les boutons en haut à droite');*/
+    window.open("presentation.html", "Bienvenue dans le game!", "height=650px, width=1000px");
+    alert('Pour jouer, appuyer sur les flèches Droite et Gauche. Pour attaquer appuyer sur Entrée. Pour sauter, appuyez sur Espace. Pour passer directement au CV ou au site, cliquez sur les boutons en haut à droite');
+    methodes.loopAnimation(0);
+}
+beginGame();
+//if inactivity
+
+function resetTimer()
+{ 
+	//merlinInitalPosition(); 
+	clearInterval(timer); 
+	secondes = 0; 
+	timer = setInterval(startTimer, 1000); 
+}
+
+window.onload = resetTimer;
+window.ontouchstart = resetTimer; 
+window.onclick = resetTimer; 
+window.onkeypress = resetTimer;
+window.onmousemove = resetTimer; 
+window.onmousedown = resetTimer;  
+
+function startTimer() 
+{ 
+	secondes++; 
+   /* merlin.style.backgroundImage=`url('assets/img/idle_inactivity.gif')`;
+        merlin.style.backgroundPositionY=`-90px`;
+        merlin.style.transform = 'scaleX(1)';
+        merlinContainer.style.width = '40px';*/
+} 

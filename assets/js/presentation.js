5b13ac725274454ade6454847c06ead48fbfa6e6
lopez <jerome_lopez@hotmail.Com>
2021-12-14 15:09:57 +0100

    presentation ifocop

--- assets/js/presentation.js
+++ assets/js/presentation.js
@@ -1,16 +1,73 @@
-let figure= document.getElementById('figure');
-        let newLeft = document.getElementById('figure').offsetLeft;
+        'use strict';
+        let figure= document.getElementById('figure');
+        let newLeft = parseFloat(document.getElementById('figure').style.marginLeft);
         let ifocop=document.getElementById("ifocop");
+        let animFigure,punch;
+        let counterPunch=0;
         let displayButton = function() {
-            ifocop.style.display="block";   
+            ifocop.style.display="block";
     }
-    setTimeout(displayButton, 8000);
-
-    ifocop.addEventListener('click',moveFigure());
-        function moveFigure(){
-            figure.style.backgroundImage=`url('assets/img/walk_anim.gif')`;
-            figure.style.width='300px';
-            newLeft=+50;
-            figure.style.left=newLeft+'px';
-            ifocop.style.display="none";
+    setTimeout(displayButton, 10000);
+    let displayPunch = function() {
+        figure.style.backgroundImage=`url('assets/img/punch_anim.gif')`;
+        figure.style.width='750px'; 
+        figure.style.zIndex=5;  
+        ifocop.remove();
+        if (counterPunch>4){
+            displayMerlin();
+            playButton();
+            setTimeout(stopAnim, 0);
+            setTimeout(stopPunch, 0);
         }
+
+
+}
+let displayMerlin = function(){
+    figure.style.backgroundImage=`url('assets/img/merlin-jump.png')`;
+       //figure.style.width='900px'; 
+       figure.style.width='100px';
+       figure.style.height='150px';
+       figure.style.marginLeft="500px";
+
+      
+}
+ifocop.addEventListener('click',moveFigure);
+        function moveFigure(){
+            let animFigure=setInterval(moveFigure2, 100);
+            
+            //figure.style.marginLeft=newLeft+'px';
+        }
+    let moveFigure2=function(){
+        if(isNaN(newLeft)) newLeft=0;
+        figure.style.backgroundImage=`url('assets/img/walk_anim.gif')`;
+        figure.style.width='320px';   
+        if(newLeft<350){
+        newLeft=newLeft+10;
+        }
+        console.log("🚀 ~ file: presentation.js ~ line 18 ~ moveFigure2 ~ newLeft", newLeft)
+        document.getElementById('figure').style.marginLeft=newLeft+"px";
+        console.log(document.getElementById('figure').style.marginLeft);
+        if (newLeft>340){
+            counterPunch=counterPunch+1;
+            let animPunch=setInterval(displayPunch,100);
+            figure.style.backgroundImage=`url('assets/img/punch_anim.gif')`;
+
+        }
+    }
+    let stopPunch=function(){
+        clearInterval(animPunch);
+    }
+    let stopAnim=function(){
+        clearInterval(animFigure);
+    }
+    let playButton =function(){
+        let buttonPlay= document.getElementById("buttonPlay");
+       buttonPlay.style.display="block";
+        buttonPlay.addEventListener("click",function(){
+            window.close();
+        })
+    }
+    
+   
+    
+    
